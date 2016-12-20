cc3ceb79ac188d3b87f2ae0289a16eda9b96eecf
Jungho Bang <me@bangtoven.com>
2016-12-20 00:28:52 -0500

    which jb are you?

---
+++ VodkaMartini-mac/BartenderNameViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BartenderNameViewController.swift
+//  VodkaMartini
+//
+//  Created by 방정호 on 2016. 12. 19..
+//  Copyright © 2016년 Bangtoven. All rights reserved.
+//
+
+import Cocoa
+
+class BartenderNameViewController: NSViewController {
+
+    @IBOutlet weak var nameTextField: NSTextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+//        
+//        let openPanel = NSOpenPanel()
+//        openPanel.allowsMultipleSelection = false
+//        openPanel.canChooseDirectories = false
+//        openPanel.canCreateDirectories = false
+//        openPanel.canChooseFiles = true
+//        openPanel.allowedFileTypes = ["mp4"]
+//        openPanel.title = "Menu"
+//        openPanel.message = "Select what you want to drink (or listen to)."
+//        let i = openPanel.runModal()
+//        if(i == NSFileHandlingPanelOKButton){
+//            print(openPanel.url)
+//            
+//        }
+    }
+    
+}
