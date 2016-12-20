c0973dbd5c042baaf2da8f886e85d5965fde569c
Jungho Bang <me@bangtoven.com>
2016-12-19 20:51:32 -0500

    Initial Commit

---
+++ VodkaMartini-mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VodkaMartini-mac
+//
+//  Created by 방정호 on 2016. 12. 19..
+//  Copyright © 2016년 Bangtoven. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
